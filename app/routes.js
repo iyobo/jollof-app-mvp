5b9a69eb1841ef1d69f2efd2f150c53703a61e88
iyobo Eki <phil.eki@gmail.com>
2018-02-28 00:17:15 -0500

    routing

--- app/routes.js
+++ app/routes.js
@@ -1,38 +1,18 @@
+const _ = require('lodash');
 const main = require('../app/controllers/mainController');
 const auth = require('../app/controllers/authController');
-const authConstraint = require('../app/constraints/auth');
-const generic = require('../app/controllers/genericApi');
+const buildGenericResourceAPI = require('./util/routeUtil').buildGenericResourceAPI;
 
-const _ = require('lodash');
+/**
+ * Use this to build routes for any model you intend to make a resource
+ */
+const resourceRoutes = {};
+//buildGenericResourceAPI(resourceRoutes, { prePath: 'v1', modelName: 'SomeModel' });
 
-function buildGenericResourceAPI(routeMap, { prePath, modelName }) {
-
-    const apiRoutes = {};
-
-    const lowerCaseName = modelName.toLowerCase();
-
-    apiRoutes[`get /api/v1/${prePath ? prePath + '/' : ''}${lowerCaseName}`] = { flow: [authConstraint.loggedIn, generic.getItems.bind(modelName, modelName)] },
-        apiRoutes[`get /api/v1/${prePath ? prePath + '/' : ''}${lowerCaseName}/:id`] = { flow: [authConstraint.loggedIn, generic.getItem.bind(modelName, modelName)] },
-        apiRoutes[`post /api/v1/${prePath ? prePath + '/' : ''}${lowerCaseName}`] = { flow: [authConstraint.loggedIn, generic.createItem.bind(modelName, modelName)] },
-        apiRoutes[`patch /api/v1/${prePath ? prePath + '/' : ''}${lowerCaseName}/:id`] = { flow: [authConstraint.loggedIn, generic.updateItem.bind(modelName, modelName)] },
-        apiRoutes[`delete /api/v1/${prePath ? prePath + '/' : ''}${lowerCaseName}/:id`] = { flow: [authConstraint.loggedIn, generic.deleteItem.bind(modelName, modelName)] },
-
-        //append to routeMap
-        _.each(apiRoutes, (v, k) => {
-            routeMap[k] = v;
-        });
-}
-
-
-let routes = {};
-
-buildGenericResourceAPI(routes, { prePath: 'organizer', modelName: 'Event' });
-buildGenericResourceAPI(routes, { prePath: 'organizer', modelName: 'Expense' });
-buildGenericResourceAPI(routes, { prePath: 'organizer', modelName: 'Ticket' });
-buildGenericResourceAPI(routes, { prePath: 'organizer', modelName: 'Investment' });
-buildGenericResourceAPI(routes, { prePath: 'organizer', modelName: 'Payee' });
-
-routes = _.merge(routes, {
+/**
+ * Put your app-specific routes here
+ */
+const appRoutes = {
     '/': { flow: main.index },
     'post /login': { flow: auth.doLogin },
     '/login': { flow: auth.login },
@@ -43,7 +23,11 @@ routes = _.merge(routes, {
     //Support
     'get /privacy-policy': { flow: main.privacyPolicy },
     'get /terms-of-use': { flow: main.termsOfUse },
-});
+};
 
 
-module.exports = routes;
+/**
+ * At the end of the day, you can do all of the above in other files.
+ * But just make sure app/routes.js exports your entire route map for your app.
+ */
+module.exports = _.merge(resourceRoutes, appRoutes);;
---
+++ app/util/routeUtil.js
@@ -0,0 +1,51 @@
+const authConstraint = require('../constraints/auth');
+const generic = require('../controllers/genericApi');
+
+function loadArray(inputMap, key) {
+    let resultArray = []
+    const val = inputMap[key];
+    if (val) {
+        if (Array.isArray(val)) {
+            resultArray = val;
+        } else {
+            resultArray = [val]
+        }
+    }
+    return resultArray;
+}
+
+/**
+ * a function to create resource routes from models. api
+ * @param routeMap - the route map
+ * @param prePath
+ * @param version
+ * @param modelName
+ * @param restConstraints - {all, getOne, get, create, update, delete}
+ */
+exports.buildGenericResourceAPI = function (routeMap, { prePath, version = 1, modelName, restConstraints = {} }) {
+
+    const apiRoutes = {};
+
+    const lowerCaseName = modelName.toLowerCase();
+
+    let baseLineConstraints = loadArray(restConstraints, 'all');
+
+    const flows = {
+        getOne: baseLineConstraints.concat(loadArray(restConstraints, 'getOne')).push(generic.getItem.bind(modelName, modelName)),
+        get: baseLineConstraints.concat(loadArray(restConstraints, 'get')).push(generic.getItems.bind(modelName, modelName)),
+        create: baseLineConstraints.concat(loadArray(restConstraints, 'create')).push(generic.createItem.bind(modelName, modelName)),
+        update: baseLineConstraints.concat(loadArray(restConstraints, 'update')).push(generic.updateItem.bind(modelName, modelName)),
+        delete: baseLineConstraints.concat(loadArray(restConstraints, 'delete')).push(generic.deleteItem.bind(modelName, modelName)),
+    }
+
+    apiRoutes[`get /api/v${version}/${prePath ? prePath + '/' : ''}${lowerCaseName}/:id`] = { flow: flows.getOne }
+    apiRoutes[`get /api/v${version}/${prePath ? prePath + '/' : ''}${lowerCaseName}`] = { flow: flows.get }
+    apiRoutes[`post /api/v${version}/${prePath ? prePath + '/' : ''}${lowerCaseName}`] = { flow: flows.create }
+    apiRoutes[`patch /api/v${version}/${prePath ? prePath + '/' : ''}${lowerCaseName}/:id`] = { flow: flows.update }
+    apiRoutes[`delete /api/v${version}/${prePath ? prePath + '/' : ''}${lowerCaseName}/:id`] = { flow: flows.delete }
+
+    //append to routeMap
+    _.each(apiRoutes, (v, k) => {
+        routeMap[k] = v;
+    });
+}

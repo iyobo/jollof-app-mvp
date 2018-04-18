4c056cfa75570c7b2d11c8e4f7689aad44c31b8e
iyobo Eki <phil.eki@gmail.com>
2018-04-18 00:35:00 -0400

    Look and feel Opti

--- frontdash/src/components/MediaList/MediaItem.jsx
+++ frontdash/src/components/MediaList/MediaItem.jsx
@@ -6,12 +6,12 @@ export default class MediaItem extends Component {
 
         return (
             <div>
-                <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-3@s uk-margin" uk-grid="">
-                    <a class="uk-card-media-left uk-cover-container" href={this.props.href}>
+                <div class="uk-card uk-card-default uk-grid-collapse uk-margin" uk-grid="">
+                    <a class="uk-card-media-left uk-cover-container uk-width-1-4@m" href={this.props.href}>
                             <img src={this.props.image || '/static/images/null.png'} alt="" uk-cover=""/>
-                            <canvas width="300" height="300"/>
+                            <canvas width="300" height="250"/>
                     </a>
-                    <div>
+                    <div className="uk-width-expand@m">
                         <div class="uk-card-body">
                             <h3 class="uk-card-title">{this.props.title}</h3>
                             <p>{this.props.info}</p>
